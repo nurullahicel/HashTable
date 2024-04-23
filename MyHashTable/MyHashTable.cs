45d3d874a9e7acba713bda7997e47b0762933618
Nurigo66 <162607187+Nurigo66@users.noreply.github.com>
2024-04-23 12:47:58 +0300

    Add project files.

---
+++ MyHashTable/MyHashTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHashTable
+{
+    public class MyHashTable
+    {
+        public string[] _hashTable;
+        public MyHashTable()
+        {
+            _hashTable = new string[10];
+        }
+        private int _hash(int key)
+        {
+            return key % _hashTable.Length;
+        }
+        public void Set(int key, string value)
+        {
+            int hashedKey = _hash(key);
+            if (_hashTable[hashedKey] != null)
+            {
+                Console.WriteLine("collision has occured");
+            }
+            else
+            {
+                _hashTable[hashedKey] = value;
+            }
+        }
+        public string Get(int key)
+        {
+            int hashedKey= _hash(key);
+            return _hashTable[hashedKey];
+        }
+    }
+}
---
+++ MyHashTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyHashTable;
+
+MyHashTable.MyHashTable mht=new MyHashTable.MyHashTable();
+
+mht.Set(1102,"1234124");
+mht.Set(1134,"321345");
+mht.Set(1595,"421344");
+mht.Set(1596,"565886");
+mht.Set(1877,"978987987");
+mht.Set(1873,"213415");
+
+
+Console.WriteLine(mht.Get(1102));
+Console.WriteLine(mht.Get(1134));
+Console.WriteLine(mht.Get(1595));
+Console.WriteLine(mht.Get(1596));
+Console.WriteLine(mht.Get(1877));
+Console.WriteLine(mht.Get(1873));
